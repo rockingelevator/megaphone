433954965938cccc84b0094b1a0fdcf5db2593df
Kirill Meshkov <rockingelevator@gmail.com>
2015-11-23 13:21:13 +0200

    upd schemas

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -4,12 +4,11 @@ from marshmallow import Schema, fields
 class UserSchema(Schema):
     password = fields.Str(load_only=True)
     class Meta:
-        fields = ("id", "username", "email", "password")
+        fields = ("id", "username", "email", "password", "avatar")
 
 
 class TeamSchema(Schema):
-    id = fields.Int()
     owner = fields.Nested(UserSchema)
-    name = fields.Str()
-    slug = fields.Str()
+    class Meta:
+        fields = ("id", "name", "slug", "owner")
 
