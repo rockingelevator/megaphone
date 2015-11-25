c81cf3cd6d73196bdd012502b6aa4e7a29bab36c
Kirill Meshkov <rockingelevator@gmail.com>
2015-11-25 13:14:11 +0200

    api url helpers

---
+++ static/scripts/components/api.js
@@ -0,0 +1,55 @@
+// url api helpers
+var request = require('request-json');
+var rootUrl = 'http://128.0.0.1:8080';
+var apiUrl = rootUrl + '/api';
+
+function ParamException(message){
+    this.message = message;
+    this.name = 'ParamException';
+};
+
+ParamException.prototype = Error.prototype;
+
+String.prototype.replaceKey=function(key, param) {
+    key = ':' + key;
+    return this.substr(0, this.indexOf(key)) + param + this.substr(this.indexOf(key)+key.length);
+}
+
+module.exports = {
+    url: function(path, params){
+        try {
+            //checking if all required params are given
+            var pathKeys = [];
+            var query = '?';
+            path.split(':').forEach(function(part){
+                pathKeys.push(part.split('/')[0]);
+            }) ;
+            pathKeys.forEach(function(key){
+                if(key != '' && !params.hasOwnProperty(key))
+                    throw new ParamException('Path param ' + key + ' is required.');
+            });
+
+            for (param in params) {
+                if (param.hasOwnProperty(length)) {
+                    var end = param.length;
+                }
+                else {
+                    throw new ParamException('Invalid param: ' + param);
+                }
+                var start = path.indexOf(':' + param);
+                if (start > -1) {
+                    path = path.replaceKey(param, params[param]);
+                }
+                else {
+                    //else put param as query part
+                    query += param + '=' + params[param] + '&';
+                }
+            }
+            return (path + query);
+        }
+        catch(e){
+            console.log(e.message, e.name);
+        }
+    }
+};
+
--- static/scripts/components/app.jsx
+++ static/scripts/components/app.jsx
@@ -3,7 +3,10 @@ var ReactDOM = require('react-dom');
 var NotificationsList = require('./notifications_list');
 var AddNotificationWidget = require('./add_notification');
 var request = require('request-json');
+var api = require('./api');
 var rootUrl = "http://127.0.0.1:8080";
+
+var client = request.createClient(rootUrl);
 
 function getTeamSlug(url){
 	var parser = document.createElement('a');
@@ -19,14 +22,22 @@ var App = React.createClass({
 		};
 	},
 	componentDidMount: function(){
-		var client = request.createClient(rootUrl);
-		var team = getTeamSlug(document.URL);
+		/*var team = getTeamSlug(document.URL);
 		var nfApiUrl = '/api/' + team + '/notifications';
 		client.get(nfApiUrl, function(err, res, data){
 			if(!err){
 				this.setState({meta: data.meta, items: data.items})
 			}
 		}.bind(this));
+		*/
+		api.url('/:team/notifications',
+				{team: 'demo-team', abc: 'wow'});
+		/*client.get(api.url('/:team/notifications',
+				{team: 'demo-team'}),
+				function(err, res, data){
+					if(!err) this.setState({meta: data.meta, items: data.items});
+				}.bind(this));*/
+
 	},
 	render: function(){
 		return <div>
